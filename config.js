3580abd00a9bbb3fbe4c80fe54423cbafe7ab453
Nick Van Strijp <nick.vanstrijp@gmail.com>
2024-06-26 14:27:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app link
-  metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
+  domainSource: "https://www.nursejobs.health", // Your WeWeb app link
+  metaDataEndpoint: "https://xnfj-dys4-m8rx.n7d.xano.io/api:dVTDQ57n/static_pay_metadata/{job_type_slug}/{geo_area_slug}", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
   patterns: {
-    dynamicPage: "/event/[^/]+"
+    dynamicPage: "/pay/[^/]+/[^/]+"
   }
 };
